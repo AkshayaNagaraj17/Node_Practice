1aeb22a6ecab53f22c82f0407118477c02bd6535
AkshayaNagaraj <cnagaraj332@gmail.com>
2024-12-16 21:55:58 +0530

    Node-1

---
+++ index.js
@@ -0,0 +1,13 @@
+// function sum(a,b)
+// {
+//     return a+b
+// }
+
+// console.log(sum(10,5))
+
+// import chalk from "chalk";
+// console.log(chalk.blue("Hello"))
+
+const path=require("path");
+console.log(__dirname);
+console.log(path.join(__dirname,"../../index.js","/Projects"))
